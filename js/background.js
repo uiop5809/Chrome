345c323643c3fab40f4443315274867517270048
parkyejin <uiop5809@naver.com>
2022-03-16 13:29:46 +0900

    background

---
+++ js/background.js
@@ -0,0 +1,18 @@
+const pictures = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.jpg",
+];
+const chosenPicture = pictures[Math.floor(Math.random() * pictures.length)];
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${chosenPicture}`;
+
+document.body.append(bgImg);
